9cbd1bba4735cc42ead397c3fb31ae207e85fdfe
Bandar197 <bandar14141489@gmail.com>
2026-08-20 21:10:50 +0300

    Fix binary_tree_depth to handle NULL tree case

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL || tree->parent == NULL)
+	if (tree == NULL)
 		return (0);
+	if (tree->parent == NULL)
+	return (0);
 
 	return (1 + binary_tree_depth(tree->parent));
 }
